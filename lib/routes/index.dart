64cf3d3356b9d030e7f031b831c07344192672a9
清净 <qingjing666666@126.com>
2026-02-12 00:11:05 +0800

    搭建基础路由和组件

---
+++ lib/routes/index.dart
@@ -0,0 +1,24 @@
+// 管理路由
+
+import 'package:flutter/material.dart';
+import 'package:hm_shop/pages/main/index.dart';
+import 'package:hm_shop/pages/login/index.dart';
+
+Widget getRootWidget(){
+  return MaterialApp(
+    // 默认页面
+    initialRoute: '/',
+    // 配置路由表
+    routes: getRootRoutes(),
+  );
+}
+
+
+Map<String, Widget Function(BuildContext)> getRootRoutes(){
+  return {
+    // 主页
+    '/': (context) => const MainPage(),
+     // 登录页
+    "/login": (context) => const LoginPage(),
+  };
+}
